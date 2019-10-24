f791e7f063b60b4fc7b9c95f34bda0acbf61a5b2
Sheroz Jumaboev <sheroz3898@gmail.com>
2019-10-24 20:43:18 +0900

    Optilam model with Backward Elimination

--- Part2_Regression/Multiple Linear Regression/multiple_linear_regression.py
+++ Part2_Regression/Multiple Linear Regression/multiple_linear_regression.py
@@ -26,3 +26,28 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+#Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+#Building the optimal model
+import statsmodels.regression.linear_model as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+X_opt = X [:,[0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit() #Step2: Fill the full model with all possible predictors(independent variables)
+regressor_OLS.summary() # Step 5 Fit model without that variable*
+
+X_opt = X [:,[0,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit() #Step3-4: Considering highest p-values. if P-value>SL -> remove it
+regressor_OLS.summary() # Step 5
+
+
+X_opt = X [:,[0,3,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit() #Step3-4: Removing Adming spend = 60%, when our SL is 5%
+regressor_OLS.summary() # Step 5
+
+X_opt = X [:,[0,3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit() #Step3-4: Removing Marketing spend = 6%, when our SL is 5%
+regressor_OLS.summary() # Step 5
+
+
